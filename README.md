3f89e4b12d77bf07d1a7dd32da122698fa3fb450
cleomarcantil <cleomarcantil@gmail.com>
2016-05-05 15:52:20 -0300

    Arquivos iniciais do projeto

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # estudo-angular2-projeto1
+
 Projeto de estudo do Angular 2
+
+
+npm install
+npm start
---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Exemplo de App Angular 2</h1>'
+})
+
+export class AppComponent { }
